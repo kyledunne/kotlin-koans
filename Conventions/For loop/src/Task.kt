5b3ba167c5db02cc8bf0a151847a83dc708f6da4
kyledunne <kyledunne@berkeley.edu>
2020-04-15 15:51:30 -0400

    a bit of the way through collections

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,21 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var currentDate = start
+
+            override fun hasNext(): Boolean {
+                return currentDate <= end
+            }
+
+            override fun next(): MyDate {
+                if (!hasNext()) throw NoSuchElementException()
+                var returnVal = currentDate
+                currentDate = currentDate.followingDate()
+                return returnVal
+            }
+        }
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -5,12 +5,21 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 // Supported intervals that might be added to dates:
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate =
+        addTimeIntervals(timeInterval, 1)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val repetitions: Int)
+
+operator fun TimeInterval.times(repetitions: Int): RepeatedTimeInterval =
+        RepeatedTimeInterval(this, repetitions)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate =
+        addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.repetitions)
